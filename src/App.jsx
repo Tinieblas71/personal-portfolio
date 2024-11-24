88ac99f50f113a852dd88c5594e6168abae56e70
Tinieblas71 <morenoestradasantiago@gmail.com>
2024-11-24 15:45:12 -0600

    Segundo Comit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Portafolio from './components/portafolio'
+
+function App() {
+
+  return (
+    <>
+      <Portafolio/>
+    </>
+  )
+
+}
+
+export default App
